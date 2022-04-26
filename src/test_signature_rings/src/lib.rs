aa25208c90867e5f24f168e72b818accd1b5da62
greatmartial <fananbang94@gmail.com>
2022-04-26 11:22:44 +0800

    [test] import ring of signature crate

--- src/test_signature_rings/src/lib.rs
+++ src/test_signature_rings/src/lib.rs
@@ -1,4 +1,48 @@
-#[ic_cdk_macros::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use std::{cell::RefCell, collections::BTreeSet};
+
+// use ring_compat::signature::{
+//     ed25519::{Signature, SigningKey, VerifyingKey},
+//     Signer, Verifier
+// };
+
+// ERROR 
+use ring;
+
+
+const PUBKEY_LEN: usize = 32;
+const PRVKEY_LEN: usize = 32;
+
+pub struct Keypair<S, V> {
+    pub private_key: S,
+    pub public_key: V,
 }
+
+pub struct User<S, V> {
+    pub name: String,
+    pub keypair: Keypair<S, V>,
+}
+
+type Users<S, V> = BTreeSet<User<S, V>>;
+
+
+thread_local! {
+    static USERS: RefCell<Users<[u8; PRVKEY_LEN], [u8; PUBKEY_LEN]>> = RefCell::default();
+}
+
+#[ic_cdk_macros::update]
+fn pem_upload() -> bool {
+    
+
+    return true;
+}
+
+#[ic_cdk_macros::query]
+fn verify_sign() -> bool {
+    return true;
+} 
+
+#[ic_cdk_macros::update]
+fn new() -> () {
+    //let rng = rand::SystemRandom::new();
+}
+
